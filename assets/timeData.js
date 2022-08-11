2d33ffecafbf363ac358d695b9490e3ef6382d5b
Eduardo <eduhmq@gmail.com>
2022-08-11 14:42:06 -0300

    js function to change html elements inner text to stored data

---
+++ assets/timeData.js
@@ -0,0 +1,31 @@
+export const time =
+{
+    "type": "module",
+    "hours":  {
+      "work": {
+        "current": 5,
+        "past": 36
+      },
+      "play": {
+        "current": 10,
+        "past": 8
+      },
+      "study": {
+        "current": 4,
+        "past": 7
+      },
+      "exercise": {
+        "current": 4,
+        "past": 5
+      },
+      "social": {
+        "current": 5,
+        "past": 10
+      },
+      "self-care": {
+        "current": 2,
+        "past": 2
+      }
+    }
+}
+
--- services/index.js
+++ services/index.js
@@ -1,8 +1,47 @@
+let time =
+{
+    
+    "work": {
+        "current": "5",
+        "past": "36"
+    },
+    "play": {
+        "current": "10",
+        "past": "8"
+    },
+    "study": {
+        "current": "4",
+        "past": "7"
+    },
+    "exercise": {
+        "current": "4",
+        "past": "5"
+    },
+    "social": {
+        "current": "5",
+        "past": "10"
+    },
+    "self": {
+        "current": "2",
+        "past": "2"
+    }
+};
+
+let workHours = time.work;
+let playHoyrs = time.play;
+let studyHours = time.study;
+let exerciseHours = time.exercise;
+let socialHours = time.social;
+let selfHours = time.self;
+
+   
+
+
 function changeTimeFrame(clicked_id) {
     let element = document.getElementById(clicked_id);
-
+    changeSchedule(clicked_id);
     elementList = document.querySelectorAll("li.time-framer");
-
+    
     for (let index = 0; index < elementList.length; index++) {
         
         if (element.id == elementList[index].id) {
@@ -12,5 +51,13 @@ function changeTimeFrame(clicked_id) {
         else {
             elementList[index].classList.remove("active")
         }
+    }
+}
+
+function changeSchedule(id) {
+    let time = id
+    if (time == "daily") {
+        console.log("test");
+        document.getElementById('work-time-information').innerText = workHours.current.concat("hrs");
     }
 }
