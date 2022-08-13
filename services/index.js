60fc0e0f1dab5611df8b8ec866281997a390c13b
Eduardo <eduhmq@gmail.com>
2022-08-12 21:11:26 -0300

    json data to get weekly and montly time

--- services/index.js
+++ services/index.js
@@ -2,6 +2,35 @@ let time =
 {
     
     "work": {
+        "current": "1",
+        "past": "8"
+    },
+    "play": {
+        "current": "2",
+        "past": "4"
+    },
+    "study": {
+        "current": "1",
+        "past": "6"
+    },
+    "exercise": {
+        "current": "6",
+        "past": "5"
+    },
+    "social": {
+        "current": "1",
+        "past": "7"
+    },
+    "self": {
+        "current": "1",
+        "past": "3"
+    }
+};
+
+let timeWeek =
+{
+    
+    "work": {
         "current": "5",
         "past": "36"
     },
@@ -27,12 +56,54 @@ let time =
     }
 };
 
+let timeMonth = {
+    
+    "work": {
+        "current": "20",
+        "past": "220"
+    },
+    "play": {
+        "current": "18",
+        "past": "28"
+    },
+    "study": {
+        "current": "15",
+        "past": "22"
+    },
+    "exercise": {
+        "current": "12",
+        "past": "41"
+    },
+    "social": {
+        "current": "31",
+        "past": "55"
+    },
+    "self": {
+        "current": "20",
+        "past": "33"
+    }
+};
+
 let workHours = time.work;
-let playHoyrs = time.play;
+let playHours = time.play;
 let studyHours = time.study;
 let exerciseHours = time.exercise;
 let socialHours = time.social;
 let selfHours = time.self;
+
+let workHoursWeekly = timeWeek.work;
+let playHoursWeekly = timeWeek.play;
+let studyHoursWeekly = timeWeek.study;
+let exerciseHoursWeekly = timeWeek.exercise;
+let socialHoursWeekly = timeWeek.social;
+let selfHoursWeekly = timeWeek.self;
+
+let workHoursMontly = timeMonth.work;
+let playHoyrsMontly = timeMonth.play;
+let studyHoursMontly = timeMonth.study;
+let exerciseHoursMontly = timeMonth.exercise;
+let socialHoursMontly = timeMonth.social;
+let selfHoursMontly = timeMonth.self;
 
 
 
@@ -60,8 +131,13 @@ changeTimeFrame("daily")
 function changeSchedule(id) {
     let time = id
     if (time == "daily") {
-        console.log("test");
         document.getElementById('work-time-information').innerText = workHours.current.concat("hrs");
+    }
+    else if (time == "weekly") {
+        document.getElementById('work-time-information').innerText = workHoursWeekly.current.concat("hrs");
+    }
+    else {
+        document.getElementById('work-time-information').innerText = workHoursMontly.current.concat("hrs")
     }
 }
 
